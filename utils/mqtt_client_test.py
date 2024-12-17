c928411d85cd285401da11d57ea1d135f8dd308b
Ustyantsev_Daniil <gikata.38@yandex.ru>
2024-12-17 10:28:28 +0300

    some fix mqtt_client

--- utils/mqtt_client_test.py
+++ utils/mqtt_client_test.py
@@ -6,14 +6,6 @@ from core.config import settings
 from crud.metric import put_metrics
 from db.db_session import db_session
 
-# MQTT_BROKER = "dev.rvts.ru"  # Адрес вашего брокера
-# MQTT_PORT = 1883  # Порт брокера
-# MQTT_TOPIC = "sensor/802241/data"  # Тема для подписки/публикации
-
-
-# Типо база данных
-metric_arr = []
-
 
 # Callback при подключении
 def on_connect(client, userdata, flags, rc):
@@ -21,20 +13,16 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe(settings.mqtt_config.topic)
 
 
+# Callback при отправке данных
 async def process_message(msg: mqtt.MQTTMessage):
     print(f"Received message: {msg.payload.decode()} on topic: {msg.topic}")
-    # Здесь вы можете добавить обработку данных, например, сохранение в базу данных
-    # await save_data_to_db(msg.payload.decode())
     session = await db_session.session_getter().__anext__()
-    print(type(session))
     await put_metrics(session=session, metrics=msg.payload.decode())
 
 
-# Обработчик сообщений
+# Обработчик сообщений (запускает асинк тк сам пахо синхронный)
 def on_message(client, userdata, msg):
     asyncio.run(process_message(msg))
-
-    # put_metrics(session=db_session.session_getter(), metrics=msg.payload.decode())
 
 
 # Создание MQTT клиента
