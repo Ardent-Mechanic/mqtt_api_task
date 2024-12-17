4ff067e6565188651d90bb91f92fc00c955c0fc2
Ustyantsev_Daniil <gikata.38@yandex.ru>
2024-12-17 11:37:42 +0300

    add time in util convert date

--- utils/date_converter.py
+++ utils/date_converter.py
@@ -1,16 +1,15 @@
 from datetime import datetime
 
 
-# Функция преобразования строки в дату
-def convert_to_date(date_str: str, format: str = "%Y-%m-%d") -> datetime.date:
+def convert_to_date(date_str: str, format: str = "%Y-%m-%d %H:%M:%S") -> datetime:
     """
-    Преобразует строку в объект datetime.date.
+    Преобразует строку в объект datetime.datetime, включая расчет времени (часы, минуты, секунды).
 
-    :param date_str: Строка с датой, например, '2024-12-17'.
-    :param format: Формат даты (по умолчанию '%Y-%m-%d').
-    :return: Объект datetime.date.
+    :param date_str: Строка с датой и временем, например, '2024-12-17 15:30:45'.
+    :param format: Формат даты и времени (по умолчанию '%Y-%m-%d %H:%M:%S').
+    :return: Объект datetime.datetime.
     """
     try:
-        return datetime.strptime(date_str, format).date()
+        return datetime.strptime(date_str, format)
     except ValueError:
-        raise ValueError(f"Неверный формат даты: '{date_str}'. Ожидался формат '{format}'.")
+        raise ValueError(f"Неверный формат даты и времени: '{date_str}'. Ожидался формат '{format}'.")
