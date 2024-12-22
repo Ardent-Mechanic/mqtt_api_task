79776a0fafa1590be5ae18647b3ffa65c3db6665
Ustyantsev_Daniil <gikata.38@yandex.ru>
2024-12-22 14:41:32 +0300

    add logging for mqtt_client

--- utils/mqtt_client_test.py
+++ utils/mqtt_client_test.py
@@ -12,6 +12,13 @@ import asyncio
 from gmqtt import Client as MQTTClient
 from fastapi_mqtt import FastMQTT, MQTTConfig
 
+import logging.config
+
+logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
+
+# Создание логгеров
+logger_mqtt = logging.getLogger("gmqtt")  # Логгер основного приложения
+
 mqtt_config = MQTTConfig(
     host=settings.mqtt_config.host,
     port=1883,
@@ -24,41 +31,38 @@ fast_mqtt = FastMQTT(config=mqtt_config)
 @fast_mqtt.on_connect()
 def connect(client: MQTTClient, flags: int, rc: int, properties: Any):
     client.subscribe("/mqtt")  # subscribing mqtt topic
-    print("Connected: ", client, flags, rc, properties)
+    logger_mqtt.info("Connected: %s, %s, %s, %s", client, flags, rc, properties)
 
 
 @fast_mqtt.subscribe(settings.mqtt_config.topic, qos=1)
 async def home_message(client: MQTTClient, topic: str, payload: bytes, qos: int, properties: Any):
-    print("temperature/humidity: ", topic, payload.decode(), qos, properties)
+    logger_mqtt.info("temperature/humidity: %s, %s, %s, %s", topic, payload.decode(), qos, properties)
 
 
 @fast_mqtt.on_message()
 async def message(client: MQTTClient, topic: str, payload: bytes, qos: int, properties: Any):
-    # print("Received message: ", topic, payload.decode(), qos, properties)
-    # session = await db_session.session_getter().__anext__()
-    # await put_metrics(session, payload.decode().split("}")[0] + "}")
     async for session in db_session.session_getter():  # Используем session_getter
         try:
             data = payload.decode().split("}")[0] + "}"
             await put_metrics(session, data)  # Обработка данных
         except Exception as e:
-            print(f"Error processing message: {e}")
+            logger_mqtt.error(f"Error processing message: {e}")
 
 
-@fast_mqtt.subscribe("my/mqtt/topic/#", qos=2)
+@fast_mqtt.subscribe(settings.mqtt_config.topic, qos=2)
 async def message_to_topic_with_high_qos(
         client: MQTTClient, topic: str, payload: bytes, qos: int, properties: Any
 ):
-    print(
+    logger_mqtt.info(
         "Received message to specific topic and QoS=2: ", topic, payload.decode(), qos, properties
     )
 
 
 @fast_mqtt.on_disconnect()
 def disconnect(client: MQTTClient, packet, exc=None):
-    print("Disconnected")
+    logger_mqtt.info("Disconnected: %s, %s", client, packet)
 
 
 @fast_mqtt.on_subscribe()
 def subscribe(client: MQTTClient, mid: int, qos: int, properties: Any):
-    print("subscribed", client, mid, qos, properties)
+    logger_mqtt.info("subscribed: %s, %s, %s, %s", client, mid, qos, properties)
